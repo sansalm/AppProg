f9d9c0f276bb0c1fe6cc372daffe9f692aa29384
sansalm <santeri.salminen.99@gmail.com>
2021-12-11 18:36:02 +0200

    Board game model and admin page

--- bgc_app/admin.py
+++ bgc_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from bgc_app.models import BoardGame
+
+admin.site.register(BoardGame)
--- bgc_app/models.py
+++ bgc_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BoardGame(models.Model):
+    """Something specific learned about a topic."""
+    #owner = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
